6b323d99f1e5b47b0276272ac206d2a0572705b4
wander-mind <w.m@tuta.com>
2024-07-11 10:45:34 +0300

    snaking

---
+++ python/01-programming-basics/01-first-steps-into-coding/02-exersise/04-vacation-books-list.py
@@ -0,0 +1,46 @@
+########################################################################
+#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::#
+#:.       ██╗    ██╗ █████╗ ███╗   ██╗██████╗ ███████╗██████╗        .:#
+#:.       ██║    ██║██╔══██╗████╗  ██║██╔══██╗██╔════╝██╔══██╗       .:#
+#:.       ██║ █╗ ██║███████║██╔██╗ ██║██║  ██║█████╗  ██████╔╝       .:#
+#:.       ██║███╗██║██╔══██║██║╚██╗██║██║  ██║██╔══╝  ██╔══██╗       .:#
+#:.       ╚███╔███╔╝██║  ██║██║ ╚████║██████╔╝███████╗██║  ██║       .:#
+#:.        ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝ ╚══════╝╚═╝  ╚═╝       .:#
+#:.                                                                  .:#
+#:.       ███╗   ███╗██╗███╗   ██╗██████╗    E-Mail: w.m@tuta.com    .:#
+#:.       ████╗ ████║██║████╗  ██║██╔══██╗                           .:#
+#:.       ██╔████╔██║██║██╔██╗ ██║██║  ██║   GitHub: github.com/     .:#
+#:.       ██║╚██╔╝██║██║██║╚██╗██║██║  ██║              wander-mind  .:#
+#:.       ██║ ╚═╝ ██║██║██║ ╚████║██████╔╝                           .:#
+#:.       ╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚═════╝      Site: wandermind.xyz  .:#
+#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::#
+########################################################################
+#
+#                        ** Summer vacation **
+#                           ** book list **
+#
+#    For his summer vacation from school, George has a list of books he
+# has to read. Since George prefers playing outside with friends, your
+# task is to calculate how many hours a day he has to spend reading, in
+# order to finish all the books on time for school.
+#
+#    Input:
+#	1. Number of pages [1-1000]
+#	2. Pages per hour [1-1000]
+#	3. Number of days to read [1-1000]
+#
+#    Output:
+#	1. Number of hours George has to spend everyday reading
+#
+
+# Input
+pages = int(input())
+pph = int(input())	# Pages per hour
+days = int(input())
+
+# Calculation
+th = pages / pph	# Total hours
+hpd = th / days		# Hours per day
+
+# Output
+print(int(hpd))
