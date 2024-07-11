e995fa0bf6c4105efb3e0c6ab3a0b47b69fb5d99
wander-mind <w.m@tuta.com>
2024-07-11 10:11:34 +0300

    snaking

--- python/01-programming-basics/01-first-steps-into-coding/02-exersise/03-deposit-calculator.py
+++ python/01-programming-basics/01-first-steps-into-coding/02-exersise/03-deposit-calculator.py
@@ -20,11 +20,11 @@
 #
 #    Write a program that calculates what amount of money you will re-
 # cieve at the end of the deposit periond at a fixed interest rate. Use
-# this formula: "sum = depositedSum + periodForDeposit * ((depositedSum *
+# this formula: "sum = depositedSum + periodForDeposit * ((depositedSum
 # * interestRate) / 12)".
 #
 #    Input:
-#	1. Deposited sum [100.00-10000.00]
+#	1. Deposited amount [100.00-10000.00]
 #	2. Deposit perion [1-12] (in months)
 #	3. Yearly interest rate [0.00-100.00]
 #
@@ -33,7 +33,14 @@
 #
 
 # Input
+init = float(input())
+period = int(input())
+interest = float(input())
 
 # Calculate
+accumulatedInterest = init * (interest / 100)
+monthlyInterest = accumulatedInterest / 12
+total = init + period * monthlyInterest
 
 # Output
+print(f"{total:.2f}")
