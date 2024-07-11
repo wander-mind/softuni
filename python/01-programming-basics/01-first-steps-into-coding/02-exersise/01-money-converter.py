c0ff160c297be916d85445d23e146e0e1a9b11de
wander-mind <w.m@tuta.com>
2024-07-11 08:33:51 +0300

    snaking

--- python/01-programming-basics/01-first-steps-into-coding/02-exersise/01-money-converter.py
+++ python/01-programming-basics/01-first-steps-into-coding/02-exersise/01-money-converter.py
@@ -19,4 +19,15 @@
 #                        ** Money converter: **
 #                           ** USD to BGN **
 #
+#    Write a program that converts currency from USD to BGN. Use the
+# fixed course of 1 USD = 1.79549 BGN.
 #
+
+# Input
+usd = float(input())
+
+# Calculation
+bgn = 1.79549 * usd
+
+# Output
+print(bgn)
