fb47ae4a305d132793255434ebffb9edc6e3e77f
wander-mind <w.m@tuta.com>
2024-07-12 09:12:10 +0300

    snaking

--- python/01-programming-basics/02-conditional-statements/01-lab/01-excellent-grade.py
+++ python/01-programming-basics/02-conditional-statements/01-lab/01-excellent-grade.py
@@ -18,4 +18,19 @@
 #
 #                         ** Excellent grade **
 #
+#    Write a program that reads your grade (R). If the grade is > 5.50
+# print "Excellent!".
 #
+#    Input:
+#	1. grade [2.00-6.00]
+#
+#    Output:
+#	1. "Excellent" [if >= 5.50]
+#
+
+# Input
+grade = float(input())
+
+# Output
+if grade >= 5.50:
+    print('Excellent!')
