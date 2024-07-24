519ef51f23d7f8573797c65b85058829666e5b24
wander-mind <w.m@tuta.com>
2024-07-24 15:58:55 +0300

    ssSSssSS...

---
+++ python/01-programming-basics/03-02-conditional-statements-adv-exercise/04-fishing-boat.py
@@ -0,0 +1,59 @@
+########################################################################
+#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::#
+#:.       ██╗    ██╗ █████╗ ███╗   ██╗██████╗ ███████╗██████╗        .:#
+#:.       ██║    ██║██╔══██╗████╗  ██║██╔══██╗██╔════╝██╔══██╗       .:#
+#:.       ██║ █╗ ██║███████║██╔██╗ ██║██║  ██║█████╗  ██████╔╝       .:#
+#:.       ██║███╗██║██╔══██║██║╚██╗██║██║  ██║██╔══╝  ██╔══██╗       .:#
+#:.       ╚███╔███╔╝██║  ██║██║ ╚████║██████╔╝███████╗██║  ██║       .:#
+#:.        ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝ ╚══════╝╚═╝  ╚═╝       .:#
+#:.                                                                  .:#
+#:.       ███╗   ███╗██╗███╗   ██╗██████╗    E-Mail: w.m@tuta.com    .:#
+#:.       ████╗ ████║██║████╗  ██║██╔══██╗                           .:#
+#:.       ██╔████╔██║██║██╔██╗ ██║██║  ██║   GitHub: github.com/     .:#
+#:.       ██║╚██╔╝██║██║██║╚██╗██║██║  ██║              wander-mind  .:#
+#:.       ██║ ╚═╝ ██║██║██║ ╚████║██████╔╝                           .:#
+#:.       ╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚═════╝      Site: wandermind.xyz  .:#
+#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::#
+########################################################################
+#
+#                          ** Fishing boat **
+#
+#    Tony and his friends love fishing soooooo much, that they decide to
+# go fishing with a ship. Price for renting that ship varies based on
+# the season and the number of fishermen. Write a program that calcula-
+# tes if they will have enough money for renting the ship.
+#
+#    Based on the season:
+#       - Spring                - 3000lv
+#       - Summer and Autumn     - 4200lv
+#       - Winter                - 2600lv
+#
+#    Based on the number of fishermen:
+#       - up to 6 ppl included              - 10% discount
+#       - between 7 and 11 ppl included     - 15% discount
+#       - over 12 ppl                       - 25% discount
+#
+#    The fishermen use and extra 5% discount on top if their number is
+# even, but only if the season isn't Autumn.
+#
+#    Input:
+#       1. Budget (whole number)
+#       2. Season [Spring, Summer, Autumn, Winter]
+#       3. Number of fishermen (Whole number)
+#
+#    Output:
+#       1. If the budget is enough:
+#           - "Yes! You have {money left [0.00]} leva left."
+#          If the budget is NOT enough:
+#           - "Not enough money! You need {money needed [0.00]} leva."
+#
+
+# Input
+bud = int(input())              # Budget
+s = input()                     # Season
+nf - int(input())               # Number of fishermen
+
+# Calculation
+
+
+# Output
